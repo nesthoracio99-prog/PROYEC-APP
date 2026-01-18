77549b51842a9a83eaac92938701cded5fb4334f
Nesthor <nesthoracio99@gmail.com>
2026-01-17 20:56:32 -0600

    Fix: target es2022 para top-level await

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import { defineConfig } from 'vite';
 // base: './' = rutas relativas (ideal para GitHub Pages, subcarpetas, etc.)
 export default defineConfig({
   base: './',
+
+  // ✅ Fix para "Top-level await is not available..."
+  build: {
+    target: 'es2022', // (o 'esnext' si quieres lo más flexible)
+  },
+  esbuild: {
+    target: 'es2022',
+  },
+
   server: {
     port: 5175,
     strictPort: true,
